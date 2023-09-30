b2b9842fe9eb25821c7b7f45ecb157bdc64f2544
tylera277 <tylera277@gmail.com>
2023-09-30 15:47:53 -0600

    working on implementing linked lists

--- Data_Structures/Linked_Lists/Linked_Lists.cpp
+++ Data_Structures/Linked_Lists/Linked_Lists.cpp
@@ -2,9 +2,47 @@
 #include <iostream>
 
 
+class Box{
+    int own_value;
+    Box* next_value;
+
+public:
+    Box::Box()
+    {
+        own_value = NULL;
+        next_value = nullptr;
+    }
+    Box::Box(int number)
+    {
+        own_value = number;
+        next_value = nullptr;
+    }
+
+    void Box::insert(int number)
+    {
+        this->next_value = new Box(number);
+
+    }
+
+    void Box::print(Box *total_list)
+    {
+        Box current = *total_list;
+
+        while(current.ptr_next_value != nullptr)
+        {
+            std::cout << current.own_value << std::endl;
+            current = current.ptr_next_value;
+        }
+    }
+
+};
+
+
 int main()
 {
-    
+    Box listerino = Box(9);
+
+    listerino.insert(12);
 
 
 
