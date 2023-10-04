952fe5207633a4355f00018049f5ec3614adca1f
tylera277 <tylera277@gmail.com>
2023-10-03 18:43:39 -0600

    linked lists example operational

--- Data_Structures/Linked_Lists/Linked_Lists.cpp
+++ Data_Structures/Linked_Lists/Linked_Lists.cpp
@@ -3,47 +3,74 @@
 
 
 class Box{
-    int own_value;
+    
+public:
+
+    int own_number_value;
     Box* next_value;
 
+    Box()
+    {
+        own_number_value = 0;
+        next_value = nullptr;
+    }
+    Box(int number)
+    {
+        own_number_value = number;
+        next_value = nullptr;
+    }
+
+   
+
+};
+
+class LinkedList:Box{
+
+    Box* currentValue;
+
 public:
-    Box::Box()
-    {
-        own_value = NULL;
-        next_value = nullptr;
-    }
-    Box::Box(int number)
-    {
-        own_value = number;
-        next_value = nullptr;
+
+    LinkedList(int number)
+    {   
+        currentValue = new Box(number);
     }
 
-    void Box::insert(int number)
-    {
-        this->next_value = new Box(number);
+    void insert(int number)
+    {   
+        Box* newValue = new Box(number);
+
+        (*newValue).next_value = currentValue;
+
+        currentValue = newValue;
 
     }
 
-    void Box::print(Box *total_list)
+    void print()
     {
-        Box current = *total_list;
+        std::cout << "List contents are: ";
 
-        while(current.ptr_next_value != nullptr)
+        while( (currentValue) != nullptr)
         {
-            std::cout << current.own_value << std::endl;
-            current = current.ptr_next_value;
+            std::cout << (*currentValue).own_number_value << ", ";
+            currentValue = currentValue->next_value;
         }
+        std::cout << std::endl;
     }
+
 
 };
 
 
 int main()
 {
-    Box listerino = Box(9);
 
-    listerino.insert(12);
+    LinkedList ll(9);
 
+    ll.insert(14);
+    ll.insert(16);
+    ll.insert(19);
+
+    ll.print();
 
 
 }
